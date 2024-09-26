2849b323ea985f1d3ca03df6fe4d689e6dba2966
alissonrhuant3 <alissonrhuant3@gmail.com>
2024-09-26 20:37:50 -0300

    Remoção

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-const dotenv = require("dotenv/config");
 const PORT = process.env.PORT || 4000;
 const dbConnect = require("./config/dbConnect");
 const morgan = require("morgan");
